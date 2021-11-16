702e29cb4e624b34071c029149406927d146f219
PenningtonStuart <stuart.lee.pennington@gmail.com>
2021-11-15 21:37:41 -0600

    added delete and put with jwt and request parsing

--- App.py
+++ App.py
@@ -1,28 +1,64 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
 
+from security import authenticate, identity
 
 app = Flask(__name__)
-app.secret_key = "Zonar" #obviously not something that should be pushed to production
+app.secret_key = "Zonar"  # obviously not something that should be pushed to production
 api = Api(app)
 
+jwt = JWT(app, authenticate, identity)  # generates a new endpoint /auth
+
 books = []
+
+
 class Book(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('author',
+                        type=str,
+                        required=True,
+                        help="This is a required field"
+                        )
+
+    @jwt_required()
     def get(self, title):
-        book = next(filter(lambda x: x['book'] == book, books), None) #using the lambda instead of and if iteration.... Can break program if there are no items in books
-        return {'book': book}, 200 if book is not None else 404 #this line can be shortened
+        book = next(filter(lambda x: x['book'] == book, books),
+                    None)  # using the lambda instead of and if iteration.... Can break program if there are no items in books
+        return {'book': book}, 200 if book is not None else 404  # this line can be shortened
 
     def post(self, title):
         if next(filter(lambda x: x['book'] == book, books), None) is not None:
-            return{'message': "An item with name '{}' already exists.".format(title)}, 400
-        data = request.get_json()
-        book = {'title': title, 'author': data['author']} #current bug when dealing with json and postman Some sort of mismatch bor json format interpretation
+            return {'message': "An item with name '{}' already exists.".format(title)}, 400
+
+        data = Book.parser.parse_args()
+
+        book = {'title': title, 'author': data[
+            'author']}  # current bug when dealing with json and postman Some sort of mismatch bor json format interpretation
         books.append(book)
         return book, 201
+
+    def delete(self, title):
+        global books
+        books = list(filter(lambda x: x['title'] != title, books))
+        return {'message': 'Item deleted'}, 200
+
+    def put(self, title):
+        data = Book.parser.parse_args()
+
+        book = next(filter(lambda x: x['book'] == book, books), None)
+        if book is None:
+            book = {'title': title, 'author': data['author']}
+            books.append(book)
+        else:
+            book.update(data)
+        return book, 200
+
 
 class BooksList(Resource):
     def get(self):
         return {'books': books}
+
 
 api.add_resource(Book, '/book/<string:title>')
 api.add_resource(BooksList, '/books')
--- security.py
+++ security.py
@@ -1,19 +1,19 @@
-
 from user import User
 
 users = [
-  User(1, 'Stuart', 'Pennington', 'stuart.lee.pennington@gmail.com', 'thisHash')
+    User(1, 'Stuart', 'Pennington', 'stuart.lee.pennington@gmail.com', 'thisHash')
 ]
-
 
 username_mapping = {u.email: u for u in users}
 
 userid_mapping = {u.id: u for u in users}
 
+
 def authenticate(email, password):
     user = username_mapping.get(email, None)
     if user and user.password == password:
         return user
+
 
 def identity(payload):
     user_id = payload['identity']
