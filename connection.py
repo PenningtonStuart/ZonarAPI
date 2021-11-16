8bc1b86650c073c17449daa02c2e68cb6c019a68
PenningtonStuart <stuart.lee.pennington@gmail.com>
2021-11-15 22:44:41 -0600

    adding sqllite and basic sql commands

---
+++ connection.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+connection = sqlite3.connect('data.db')
+
+cursor = connection.cursor()
+
+create_table = "CREATE TABLE user (id int, first_name text, last_name text, email text, password text)"
+cursor.execute(create_table)
+
+users = [
+    (1, 'Katie', 'Pennington', 'katie.pennington@gmail.com', 'asdfg'),
+    (1, 'Katie', 'Pennington', 'katie.pennington@gmail.com', 'asdfg')
+]
+insert_query = "INSERT INTO user VALUES(?, ?, ?, ?, ?)"
+cursor.executemany(insert_query, users)
+
+select_query = "SELECT * FROM users"
+for row in cursor.execute(select_query):
+    print(row)
+
+connection.commit()
+
+connection.close()
