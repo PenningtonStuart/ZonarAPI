8cde2f34e311295b89e55e2339bed8d39e58dbd4
PenningtonStuart <stuart.lee.pennington@gmail.com>
2021-11-15 20:29:24 -0600

    added basic auth infra

--- App.py
+++ App.py
@@ -1,19 +1,22 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
+
 app = Flask(__name__)
+app.secret_key = "Zonar" #obviously not something that should be pushed to production
 api = Api(app)
 
 books = []
 class Book(Resource):
     def get(self, title):
-        for book in books:
-            if book['title'] == title:
-                return book
-        return {'book': None}, 404
+        book = next(filter(lambda x: x['book'] == book, books), None) #using the lambda instead of and if iteration.... Can break program if there are no items in books
+        return {'book': book}, 200 if book is not None else 404 #this line can be shortened
 
     def post(self, title):
-        book = {'title': 'Treasure Island', 'author': 'Jules Vern'}
+        if next(filter(lambda x: x['book'] == book, books), None) is not None:
+            return{'message': "An item with name '{}' already exists.".format(title)}, 400
+        data = request.get_json()
+        book = {'title': title, 'author': data['author']} #current bug when dealing with json and postman Some sort of mismatch bor json format interpretation
         books.append(book)
         return book, 201
 
